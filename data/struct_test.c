46f35ee2136dd201e7e6fc55b2e8e290a46c6be3
MasterFHC <MasterFHC@sjtu.edu.cn>
2024-11-23 17:45:36 +0800

    fixed every existing testcase

--- data/struct_test.c
+++ data/struct_test.c
@@ -1,28 +1,32 @@
 struct Test_int
 {
-    int Num_1;
-    int Num_2,Num_3;
-};
+    int Num_1 ;
+    int Num_2 ;
+    int Num_3 ;
+} ;
 
 struct Test_int_pointer
 {
-    int *ptr_1;
-    int *ptr_2,Num_1;
-};
+    int * ptr_1 ;
+    int * ptr_2 ;
+    int Num_1 ;
+} ;
 
 struct Test_char
 {
-    char c_1;
-    char c_2,c_3;
-};
+    char c_1 ;
+    char c_2 ; 
+    char c_3 ;
+} ;
 
 struct Test_char_pointer
 {
-    char *ptr_1;
-    char *ptr_2,c_1;
-};
+    char * ptr_1 ;
+    char * ptr_2 ;
+    char c_1 ;
+} ;
 
 struct Empty_Struct
 {
-    //Empty
-};
+    
+} ;
