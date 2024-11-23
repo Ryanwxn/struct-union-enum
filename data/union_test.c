413644a54e9286576b4fa891e3b181b6e9e015e7
IronCarbonate <2140188204@qq.com>
2024-11-23 14:22:44 +0800

    add union,struct test

---
+++ data/union_test.c
@@ -0,0 +1,23 @@
+union Test_int
+{
+    int Num_1;
+    int Num_2,Num_3;
+};
+
+union Test_int_pointer
+{
+    int *ptr_1;
+    int *ptr_2,Num_1;
+};
+
+union Test_char
+{
+    char c_1;
+    char c_2,c_3;
+};
+
+union Test_char_pointer
+{
+    char *ptr_1;
+    char *ptr_2,c_1;
+};
