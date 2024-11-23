68e3738f0285cbc26cd85bc4fb475481d2378d51
MasterFHC <MasterFHC@sjtu.edu.cn>
2024-11-23 18:38:03 +0800

    added a crude bash test program (driver.py)

--- data/struct_test.c
+++ data/struct_test.c
@@ -1,32 +1,32 @@
 struct Test_int
 {
-    int Num_1 ;
-    int Num_2 ;
-    int Num_3 ;
-} ;
+    int Num_1;
+    int Num_2;
+    int Num_3;
+};
 
 struct Test_int_pointer
 {
-    int * ptr_1 ;
-    int * ptr_2 ;
-    int Num_1 ;
-} ;
+    int *ptr_1;
+    int *ptr_2;
+    int Num_1;
+};
 
 struct Test_char
 {
-    char c_1 ;
-    char c_2 ; 
-    char c_3 ;
-} ;
+    char c_1;
+    char c_2; 
+    char c_3;
+};
 
 struct Test_char_pointer
 {
-    char * ptr_1 ;
-    char * ptr_2 ;
-    char c_1 ;
-} ;
+    char *ptr_1;
+    char *ptr_2;
+    char c_1;
+};
 
 struct Empty_Struct
 {
     
-} ;
+};
