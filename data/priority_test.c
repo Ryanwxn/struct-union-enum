ec279d4fba2b85dcbad064783e5c3b1493a923ab
Yuxiang Wu <53373074+Vxlimo@users.noreply.github.com>
2024-11-28 22:30:14 +0800

    modify priority_test.c

--- data/priority_test.c
+++ data/priority_test.c
@@ -5,10 +5,8 @@ int (*ptr_arr_i)[10];
 int* arr_ptr_i[10];
 
 int func(int);
-int (*ptr_func)(int);
-int* func_ptr(int);
-int *func_arr(int)[10];
-int (*func_arr(int))[10];
-int (*(*ptr_func_arr)(int))[10];
-int* (*fun_arr_ptr(int))[10];
-int* (*(*ptr_func_arr_ptr)(int))[10];
+int *ptr_func(int);
+int (*func_ptr)(int);
+int func_arr(int)[10];
+int *ptr_arr_func(int)[10];
+int (*arr_func_ptr)(int)[10];
